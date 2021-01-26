dce461b61f7aaa41fb07d193410b8e02ee4730ff
ibartuszek <istvan.bartuszek@gmail.com>
2021-01-26 16:19:52 +0100

    vaadin: add message filtering on MainView

--- src/main/java/com/example/playground/view/MainView.java
+++ src/main/java/com/example/playground/view/MainView.java
@@ -4,7 +4,13 @@ import com.example.playground.dal.mongo.ExampleEntry;
 import com.example.playground.dal.mongo.repository.ExampleRepository;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Route("/index.html")
@@ -13,15 +19,41 @@ public class MainView extends VerticalLayout {
     private final ExampleRepository repository;
     private final Grid<ExampleEntry> grid;
 
-    public MainView(ExampleRepository repository) {
+    private String messageFilter = "";
+
+    MainView(ExampleRepository repository) {
         this.repository = repository;
-        this.grid = new Grid<>(ExampleEntry.class);
+        this.grid = createGrid();
+
+        add(createMessageFilter());
         add(grid);
-        listEntries();
     }
 
-    private void listEntries() {
+    private Grid<ExampleEntry> createGrid() {
+        Grid<ExampleEntry> grid = new Grid<>(ExampleEntry.class);
         grid.setItems(repository.findAll());
+        return grid;
+    }
+
+    private TextField createMessageFilter() {
+        TextField messageFilter = new TextField();
+        messageFilter.setPlaceholder("Filter to message");
+        messageFilter.setValueChangeMode(ValueChangeMode.EAGER);
+        messageFilter.addValueChangeListener(e -> updateListOnMessage(e.getValue()));
+        return messageFilter;
+    }
+
+    private void updateListOnMessage(String messageFilter) {
+        this.messageFilter = messageFilter;
+        if (!StringUtils.isEmpty(messageFilter)) {
+            grid.setItems(getFilteredList());
+        }
+    }
+
+    private List<ExampleEntry> getFilteredList() {
+        return repository.findAll().stream()
+                .filter(entry -> entry.getMessage().contains(messageFilter))
+                .collect(Collectors.toList());
     }
 
 }
