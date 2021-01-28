0cc4e99366e5fe19c6f4f0ea30a57e649091090b
ibartuszek <istvan.bartuszek@gmail.com>
2021-01-28 20:00:22 +0100

    vaadin: add ExampleModelEditor to MainView replace entry and repository to model and service

--- src/main/java/com/example/playground/view/MainView.java
+++ src/main/java/com/example/playground/view/MainView.java
@@ -1,19 +1,19 @@
 package com.example.playground.view;
 
-import com.example.playground.dal.mongo.ExampleEntry;
-import com.example.playground.dal.mongo.repository.ExampleRepository;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.datetimepicker.DateTimePicker;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
-import org.apache.commons.lang3.StringUtils;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,23 +21,43 @@ import java.util.stream.Collectors;
 @Route("/index.html")
 public class MainView extends VerticalLayout {
 
-    private final ExampleRepository repository;
-    private final Grid<ExampleEntry> grid;
+    private final ExampleService service;
+    private final Grid<ExampleModel> grid;
+    private final ExampleModelEditor modelEditor;
 
     private String messageFilter = "";
-    private Instant timestampFilter = Instant.EPOCH;
+    private LocalDateTime timestampFilter = LocalDateTime.MIN;
+    private String zoneId = "UTC";
 
-    MainView(ExampleRepository repository) {
-        this.repository = repository;
+    MainView(ExampleService service, ExampleModelEditor modelEditor) {
+        this.service = service;
+        this.modelEditor = modelEditor;
+        UI.getCurrent().getPage().retrieveExtendedClientDetails(extendedClientDetails -> {
+            this.zoneId = extendedClientDetails.getTimeZoneId();
+        });
+        this.modelEditor.setZoneId(this.zoneId);
+        this.modelEditor.setChangeHandler(() -> {
+            modelEditor.setVisible(false);
+            getFilteredList();
+        });
         this.grid = createGrid();
 
         add(createFilterBar());
         add(grid);
+        add(this.modelEditor);
     }
 
-    private Grid<ExampleEntry> createGrid() {
-        Grid<ExampleEntry> grid = new Grid<>(ExampleEntry.class);
-        grid.setItems(repository.findAll());
+    private Grid<ExampleModel> createGrid() {
+        Grid<ExampleModel> grid = new Grid<>(ExampleModel.class);
+
+        grid.setHeight("300px");
+        grid.setColumns("id", "message", "timeStamp", "date");
+        grid.getColumnByKey("id").setWidth("50px").setFlexGrow(0);
+
+        // edit
+        grid.asSingleSelect().addValueChangeListener(e -> modelEditor.editModel(e.getValue()));
+
+        grid.setItems(service.findAll(zoneId));
         return grid;
     }
 
@@ -46,12 +66,13 @@ public class MainView extends VerticalLayout {
         bar.setAlignItems(Alignment.START);
         bar.add(createMessageFilter());
         bar.add(createDateTimePicker());
+        bar.add(createAddModelButton());
         return bar;
     }
 
     private TextField createMessageFilter() {
         TextField messageFilter = new TextField();
-        messageFilter.setLabel("Entries which contains:");
+        messageFilter.setLabel("Models which contains:");
         messageFilter.setPlaceholder("Filter to message");
         messageFilter.setValueChangeMode(ValueChangeMode.EAGER);
         messageFilter.addValueChangeListener(e -> updateListOnMessage(e.getValue()));
@@ -60,32 +81,42 @@ public class MainView extends VerticalLayout {
 
     private void updateListOnMessage(String messageFilter) {
         this.messageFilter = messageFilter;
-        if (!StringUtils.isEmpty(messageFilter)) {
+        if (this.messageFilter != null) {
             grid.setItems(getFilteredList());
         }
     }
 
-    private List<ExampleEntry> getFilteredList() {
-        List<ExampleEntry> exampleEntryList = timestampFilter.isAfter(Instant.EPOCH)
-                ? repository.findAllByTimeStampAfter(timestampFilter)
-                : repository.findAll();
-        return exampleEntryList.stream()
-                .filter(entry -> entry.getMessage().contains(messageFilter))
+    private List<ExampleModel> getFilteredList() {
+        List<ExampleModel> exampleModelList = timestampFilter.isAfter(LocalDateTime.MIN)
+                ? service.findAllAfter(timestampFilter, zoneId)
+                : service.findAll(zoneId);
+        return exampleModelList.stream()
+                .filter(model -> model.getMessage().contains(messageFilter))
                 .collect(Collectors.toList());
     }
 
     private DateTimePicker createDateTimePicker() {
         DateTimePicker dateTimePicker = new DateTimePicker();
-        dateTimePicker.setLabel("Entries after timestamp (UTC):");
+        dateTimePicker.setLabel("Models after timestamp (UTC):");
         dateTimePicker.addValueChangeListener(e -> updateListOnDate(e.getValue()));
         return dateTimePicker;
     }
 
     private void updateListOnDate(LocalDateTime dateTime) {
         if (dateTime != null) {
-            timestampFilter = dateTime.toInstant(ZoneOffset.UTC);
+            timestampFilter = dateTime;
             grid.setItems(getFilteredList());
         }
+    }
+
+    private Div createAddModelButton() {
+        Div container = new Div();
+        Label label = new Label();
+        label.setText("To create and edit an model");
+        Button button = new Button("New model", VaadinIcon.PLUS.create());
+        button.addClickListener(e -> modelEditor.addNewModel());
+        container.add(label, button);
+        return container;
     }
 
 }
